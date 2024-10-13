19ce7d75b2e57570bb9fa317ed0b95878238f83f
KevinShorts <119091481+KevinShorts@users.noreply.github.com>
2024-10-13 12:14:24 +0700

    final.c

--- maincs.c
+++ maincs.c
@@ -1,42 +1,42 @@
 // Ứng dụng tính toán chỉ số sức khỏe.
-// BMI Calcuator
-// Metric Units
-# include <stdio.h>
+// BMI Calculator
+#include <stdio.h>
 
 int main() {
+  float weight, height, bmi;
+  char gender;
   printf("= = = = = = = = = = = = = = = = = = = = = = = = = =");
   printf("\nWelcome to BMI Calculator.");
   printf("\n= = = = = = = = = = = = = = = = = = = = = = = = = =");
-  printf("\nPlease enter your name: ");
-  float weight, height, bmi;
-  char gender;
+  printf("\nPlease fill in the form below.");
+  printf("\nEnter your name: ");
   char name[100];
   scanf("%s", name);
-  printf("Please enter your weight (kg): ");
-  scanf("%f", &weight);
-  printf("Please enter your height (cm): ");
-  scanf("%f", &height);
-  height = height / 100;
-  printf("Please enter your gender (M/F): ");
+  printf("Enter your gender (M/F): ");
   scanf(" %c", &gender);
+  printf("Enter your weight (kg): ");
+  scanf("%f", &weight);
+  printf("Enter your height (m): ");
+  scanf("%f", &height);
   bmi = weight / (height * height);
-  printf("\nHere is your information. \n");
-  printf("\nYour name is %s", name);
-  printf("\nYour weight is %.2f kg", weight);
-  printf("\nYour height is %.2f cm", height);
-  printf("\nYour gender is %c\n", gender);
-  printf("\nHere is the result.\n");
-  printf("\nYour BMI is: %.2f kg/m^2", bmi);
+  printf("= = = = = = = = = = = = = = = = = = = = = = = = = =");
+  printf("\nHere is your information:");
+  printf("\nName: %s", name);
+  printf("\nGender: %c", gender);
+  printf("\nWeight: %.2f kg", weight);
+  printf("\nHeight: %.2f m", height);
+  printf("\n= = = = = = = = = = = = = = = = = = = = = = = = = =");
+  printf("\nHere is the result:");
+  printf("\nBMI: %.2f kg/m^2", bmi);
   if (bmi < 18.5) {
     printf("\nYou are underweight.");
-  } else if (bmi >= 18.5 && bmi <= 24.9) {
-    printf("\nYou are normal.");
-  } else if (bmi >= 25 && bmi <= 29.9) {
-    printf("\nYou are Obese (Class 1).");
-  } else if (bmi >= 30 && bmi <= 34.9) {
-    printf("\nYou are Obese (Class 2).");
+  } else if (bmi >= 18.5 && bmi < 25) {
+    printf("\nYou are normal weight.");
+  } else if (bmi >= 25 && bmi < 30) {
+    printf("\nYou are overweight.");
   } else {
-    printf("\nYou are Obese (Class 3 or serve obesity).");
+    printf("\nYou are obese.");
   }
+  printf("\n= = = = = = = = = = = = = = = = = = = = = = = = = =");
   return 0;
 }
